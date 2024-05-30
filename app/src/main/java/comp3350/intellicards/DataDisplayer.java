26a7678ff373e28329e48f878492d28d5b490326
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2024-05-30 17:44:21 -0500

    Change better variable names

--- app/src/main/java/comp3350/intellicards/DataDisplayer.java
+++ app/src/main/java/comp3350/intellicards/DataDisplayer.java
@@ -19,17 +19,17 @@ public class DataDisplayer extends RecyclerView.Adapter<DataDisplayer.ViewHolder
      * (custom ViewHolder)
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
-        private final TextView textView;
+        private final TextView flashCardTextRecycle;
 
         public ViewHolder(View view) {
             super(view);
             // Define click listener for the ViewHolder's View
 
-            textView = (TextView) view.findViewById(R.id.textView);
+            flashCardTextRecycle = (TextView) view.findViewById(R.id.flashCardTextRecycle);
         }
 
         public TextView getTextView() {
-            return textView;
+            return flashCardTextRecycle;
         }
     }
 
