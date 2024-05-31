aab2f660c2b8ce0d56c5352625bfcd1562aec93a
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2024-05-30 19:59:22 -0500

    add Delete function

--- app/src/main/java/comp3350/intellicards/DataDisplayer.java
+++ app/src/main/java/comp3350/intellicards/DataDisplayer.java
@@ -3,16 +3,23 @@ package comp3350.intellicards;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import comp3350.intellicards.R;
 
 public class DataDisplayer extends RecyclerView.Adapter<DataDisplayer.ViewHolder> {
 
-    private String[] localDataSet;
+    private FlashCardSet flashCardSet;
+
+    private AdapterView.OnItemClickListener deleteButtonClick;
+
 
     /**
      * Provide a reference to the type of views that you are using
@@ -20,27 +27,50 @@ public class DataDisplayer extends RecyclerView.Adapter<DataDisplayer.ViewHolder
      */
     public static class ViewHolder extends RecyclerView.ViewHolder {
         private final TextView flashCardTextRecycle;
+        private final Button deleteButton;
 
-        public ViewHolder(View view) {
+        public ViewHolder(View view, AdapterView.OnItemClickListener deleteButtonClick, FlashCardSet flashCardSet) {
             super(view);
             // Define click listener for the ViewHolder's View
-
             flashCardTextRecycle = (TextView) view.findViewById(R.id.flashCardTextRecycle);
+            deleteButton = (Button) view.findViewById(R.id.deleteButton);
+
+            //Clicking this will mark the cooresponding flashcard as deleted
+            // and it will not pop up as a flashcard until restored
+            deleteButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                       FlashCard card = flashCardSet.getFlashCardById((String)deleteButton.getTag());
+                       card.markDeleted();
+
+                }
+            });
+
         }
 
         public TextView getTextView() {
             return flashCardTextRecycle;
         }
+
+        public Button deleteButton() {
+            return deleteButton;
+        }
+
+        public View getView()
+        {
+            return super.itemView.getRootView();
+        }
+
     }
 
     /**
      * Initialize the dataset of the Adapter
      *
-     * @param dataSet String[] containing the data to populate views to be used
+     * @param flashCards contain the flashcards the data to populate views to be used
      * by RecyclerView
      */
-    public DataDisplayer(String[] dataSet) {
-        localDataSet = dataSet;
+    public DataDisplayer(FlashCardSet flashCards) {
+        flashCardSet = flashCards;
     }
 
     // Create new views (invoked by the layout manager)
@@ -51,7 +81,7 @@ public class DataDisplayer extends RecyclerView.Adapter<DataDisplayer.ViewHolder
         View view = LayoutInflater.from(viewGroup.getContext())
                 .inflate(R.layout.text_row_item, viewGroup, false);
 
-        return new ViewHolder(view);
+        return new ViewHolder(view,deleteButtonClick,flashCardSet);
     }
 
     // Replace the contents of a view (invoked by the layout manager)
@@ -60,13 +90,29 @@ public class DataDisplayer extends RecyclerView.Adapter<DataDisplayer.ViewHolder
 
         // Get element from your dataset at this position and replace the
         // contents of the view with that element
-        viewHolder.getTextView().setText(localDataSet[position]);
+        FlashCard card = flashCardSet.getFlashcards().get(position);
+        if (card.isDeleted())
+        {
+            ViewGroup parentView = ((ViewGroup) viewHolder.getTextView().getParent());
+            parentView.removeView(viewHolder.getTextView());
+            parentView.removeView(viewHolder.deleteButton());
+            ViewGroup parentContainer = (ViewGroup) parentView.getParent();
+            if (parentContainer != null) {
+                parentContainer.removeView(parentView);
+            }
+
+        } else
+        {
+            viewHolder.getTextView().setText(card.toString());
+            viewHolder.deleteButton().setTag(card.getUuid());
+        }
+
     }
 
     // Return the size of your dataset (invoked by the layout manager)
     @Override
     public int getItemCount() {
-        return localDataSet.length;
+        return flashCardSet.getFlashcards().size();
     }
 }
 
