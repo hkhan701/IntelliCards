af3eb0457e2fcb5ce8144cdbf4e23961c5fb5f26
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-06-02 21:40:15 -0500

    Refactor FlashcardSet
    To get all flashcards that have not been deleted the method is renamed to getActiveFlashcards. Added @NonNull to params

--- app/src/main/java/comp3350/intellicards/Objects/FlashcardSet.java
+++ app/src/main/java/comp3350/intellicards/Objects/FlashcardSet.java
@@ -8,7 +8,6 @@ import java.util.UUID;
 
 public class FlashcardSet {
     private final UUID uuid;
-
     private String flashcardSetName;
     private List<Flashcard> flashcards;
 
@@ -31,17 +30,15 @@ public class FlashcardSet {
         return uuid;
     }
 
-    public void addFlashCard(Flashcard flashcard) {
+    public void addFlashCard(@NonNull Flashcard flashcard) {
         flashcards.add(flashcard);
     }
 
-    //return the undeleted flashcards
-    public FlashcardSet getFlashcards() {
+    // Return a flashcard set that contains only the active flashcards
+    public FlashcardSet getActiveFlashcards() {
         FlashcardSet undeletedCards = new FlashcardSet();
-        for(Flashcard card:flashcards)
-        {
-            if(!card.isDeleted())
-            {
+        for (Flashcard card : flashcards) {
+            if (!card.isDeleted()) {
                 undeletedCards.addFlashCard(card);
             }
         }
@@ -49,31 +46,26 @@ public class FlashcardSet {
         return undeletedCards;
     }
 
-    //return deleted flashcards
-    public FlashcardSet getDeletedFlashCards()
-    {
+    // Return all the flashcards that have been deleted in this set
+    public FlashcardSet getDeletedFlashCards() {
         FlashcardSet deletedCards = new FlashcardSet();
-        for(Flashcard card:flashcards)
-        {
-            if(card.isDeleted())
-            {
+        for (Flashcard card : flashcards) {
+            if (card.isDeleted()) {
                 deletedCards.addFlashCard(card);
             }
         }
         return deletedCards;
     }
 
-
     public String getFlashCardSetName() {
         return flashcardSetName;
     }
 
-    public void setFlashCardSetName(String name) {
+    public void setFlashCardSetName(@NonNull String name) {
         this.flashcardSetName = name;
     }
 
-    public Flashcard getFlashCardById(UUID uuid)
-    {
+    public Flashcard getFlashCardById(UUID uuid) {
         for (Flashcard card : flashcards) {
             if (card.getUUID().equals(uuid)) {
                 return card;
@@ -82,17 +74,13 @@ public class FlashcardSet {
         return null;
     }
 
-    public Flashcard getIndex(int index)
-    {
+    public Flashcard getIndex(int index) {
         return flashcards.get(index);
     }
 
-    public int size()
-    {
+    public int size() {
         return flashcards.size();
     }
-
-
 
     @NonNull
     @Override
