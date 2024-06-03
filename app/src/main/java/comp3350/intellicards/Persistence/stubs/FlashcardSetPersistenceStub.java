34518e8793a71ad6efb58d83fbaab29b0fd90125
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-06-02 21:30:36 -0500

    Implementation for FlashcardSetPersistence
    Stub implementing the methods that will be used to manipulate the database

---
+++ app/src/main/java/comp3350/intellicards/Persistence/stubs/FlashcardSetPersistenceStub.java
@@ -0,0 +1,56 @@
+package comp3350.intellicards.Persistence.stubs;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+
+import comp3350.intellicards.Objects.Flashcard;
+import comp3350.intellicards.Objects.FlashcardSet;
+import comp3350.intellicards.Persistence.FlashcardSetPersistence;
+
+public class FlashcardSetPersistenceStub implements FlashcardSetPersistence {
+
+    private Map<UUID, FlashcardSet> flashcardSets;
+
+    public FlashcardSetPersistenceStub() {
+        flashcardSets = new HashMap<>();
+    }
+
+    @Override
+    public FlashcardSet getFlashcardSet(FlashcardSet currentFlashcardSet) {
+        return flashcardSets.get(currentFlashcardSet.getUUID());
+    }
+
+    // return a flashcard set with flashcards marked as deleted removed
+    public FlashcardSet getActiveFlashcardSet(FlashcardSet currentFlashcardSet) {
+
+        FlashcardSet set = flashcardSets.get(currentFlashcardSet.getUUID());
+        if (set != null) {
+            return set.getActiveFlashcards();
+        }
+        return null;
+    }
+
+    @Override
+    public List<FlashcardSet> getAllFlashcardSets() {
+        return Collections.unmodifiableList(new ArrayList<>(flashcardSets.values()));
+    }
+
+    public boolean addFlashCardToFlashcardSet(FlashcardSet flashcardSet, Flashcard flashcard) {
+        FlashcardSet set = flashcardSets.get(flashcardSet.getUUID());
+        if (set != null) {
+            set.addFlashCard(flashcard);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public FlashcardSet insertFlashcardSet(FlashcardSet newFlashcardSet) {
+        flashcardSets.put(newFlashcardSet.getUUID(), newFlashcardSet);
+        return newFlashcardSet;
+    }
+}
