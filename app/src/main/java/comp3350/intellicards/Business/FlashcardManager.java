62f3f4c178a3305b2957014e7612fd6ca1d27801
Jade Lee <jadelee0924@gmail.com>
2024-06-05 23:49:42 -0500

    Add missing methods in FlashcardManager.java
    getAllActiveFlashcards, getAllDeletedFlashcards, and getFlashcard

--- app/src/main/java/comp3350/intellicards/Business/FlashcardManager.java
+++ app/src/main/java/comp3350/intellicards/Business/FlashcardManager.java
@@ -1,28 +1,46 @@
 package comp3350.intellicards.Business;
-import java.util.List;import comp3350.intellicards.Objects.Flashcard;
+
+import java.util.List;
+
+import comp3350.intellicards.Objects.Flashcard;
 import comp3350.intellicards.Persistence.FlashcardPersistence;
 import comp3350.intellicards.Persistence.stubs.FlashcardPersistenceStub;
 
 public class FlashcardManager {
     private FlashcardPersistence flashcardPersistence;
-    private Flashcard flashcard;
+
     public FlashcardManager() {
         flashcardPersistence = new FlashcardPersistenceStub();
-        flashcard = null;
     }
+
     public FlashcardManager(final FlashcardPersistence flashcardPersistence) {
         this.flashcardPersistence = flashcardPersistence;
-        flashcard = null;
     }
+
+    List<Flashcard> getAllActiveFlashcards() {
+        return this.flashcardPersistence.getAllActiveFlashcards();
+    }
+
+    List<Flashcard> getAllDeletedFlashcards() {
+        return this.getAllDeletedFlashcards();
+    }
+
+    Flashcard getFlashcard(String id) {
+        return this.getFlashcard(id);
+    }
+
     public Flashcard insertFlashcard(Flashcard currFlashcard) {
         return flashcardPersistence.insertFlashcard(currFlashcard);
     }
+
     public Flashcard updateFlashcard(Flashcard currFlashcard) {
         return flashcardPersistence.updateFlashcard(currFlashcard);
     }
+
     boolean markFlashcardAsDeleted(String id) {
         return flashcardPersistence.markFlashcardAsDeleted(id);
     }
+
     boolean restoreFlashcard(String id) {
         return flashcardPersistence.restoreFlashcard(id);
     }
