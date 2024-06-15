7ab990489a51fa242f0d14e7e0b6674fc9326883
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-06-14 21:30:05 -0500

    Remove all flashcard creation code from main activity

--- app/src/main/java/comp3350/intellicards/Presentation/MainActivity.java
+++ app/src/main/java/comp3350/intellicards/Presentation/MainActivity.java
@@ -3,29 +3,17 @@ package comp3350.intellicards.Presentation;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.GridLayout;
-import android.widget.Spinner;
-import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 
-import java.util.ArrayList;
 import java.util.List;
 
-import comp3350.intellicards.Objects.Flashcard;
 import comp3350.intellicards.Objects.FlashcardSet;
-
-import comp3350.intellicards.Business.FlashcardManager;
 import comp3350.intellicards.Business.FlashcardSetManager;
-
 import comp3350.intellicards.Business.StubManager;
-
 import comp3350.intellicards.R;
 
 public class MainActivity extends Activity {
@@ -33,25 +21,14 @@ public class MainActivity extends Activity {
     private FlashcardSetManager flashcardSetManager;
     private GridLayout gridLayout;
 
-    // This can be moved to when we create the flashcard page
-    // Anything related to creating the flashcard should be moved from this main activity
-    private FlashcardSet selectedFlashcardSet;
-    private List<FlashcardSet> flashcardSets;
-    private ArrayAdapter<String> adapter;
-    private FlashcardManager flashcardManager;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         initializePersistence();
-
-        setupFlashcardSetSpinner();
-        setupSubmitButton();
         profilePageButtonOnClick();
         createNewSetButtonOnClick();
-
     }
 
     private void initializePersistence() {
@@ -59,7 +36,6 @@ public class MainActivity extends Activity {
             StubManager.initializeStubData();
         }
 
-        flashcardManager = new FlashcardManager(StubManager.getFlashcardPersistence());
         flashcardSetManager = new FlashcardSetManager(StubManager.getFlashcardSetPersistence());
 
         gridLayout = findViewById(R.id.gridLayout);
@@ -76,18 +52,12 @@ public class MainActivity extends Activity {
             Button FlashcardSetButton = new Button(this);
             String title = set.getFlashcardSetName() + " (" + set.getActiveCount() + ")";
             FlashcardSetButton.setText(title);
-            FlashcardSetButton.setLayoutParams(new GridLayout.LayoutParams(
-                    GridLayout.spec(GridLayout.UNDEFINED, 1f),
-                    GridLayout.spec(GridLayout.UNDEFINED, 1f)
-            ));
+            FlashcardSetButton.setLayoutParams(new GridLayout.LayoutParams(GridLayout.spec(GridLayout.UNDEFINED, 1f), GridLayout.spec(GridLayout.UNDEFINED, 1f)));
             FlashcardSetButton.setPadding(16, 16, 16, 16);
-            FlashcardSetButton.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View v) {
-                    Intent intent = new Intent(MainActivity.this, FlashcardSetActivity.class);
-                    intent.putExtra("flashcardSetUUID", set.getUUID());
-                    startActivity(intent);
-                }
+            FlashcardSetButton.setOnClickListener(v -> {
+                Intent intent = new Intent(MainActivity.this, FlashcardSetActivity.class);
+                intent.putExtra("flashcardSetUUID", set.getUUID());
+                startActivity(intent);
             });
             gridLayout.addView(FlashcardSetButton);
         }
@@ -96,74 +66,16 @@ public class MainActivity extends Activity {
     // Show a dialog to create a new Flashcard Set
     private void showCreateNewSetDialog() {
         EditText newSetNameInput = new EditText(this);
-        new android.app.AlertDialog.Builder(this)
-                .setTitle("Create New Flashcard Set")
-                .setMessage("Enter the name for the new Flashcard Set:")
-                .setView(newSetNameInput)
-                .setPositiveButton("Create", (dialog, whichButton) -> {
-                    String newSetName = newSetNameInput.getText().toString().trim();
-                    if (!newSetName.isEmpty()) {
-                        FlashcardSet newFlashcardSet = new FlashcardSet(newSetName);
-                        flashcardSetManager.insertFlashcardSet(newFlashcardSet);
-                        loadFlashcardSets(); // Refresh the list of Flashcard Sets
-                        setupFlashcardSetSpinner(); // Update the spinner with the new Flashcard Set
-                    } else {
-                        dialog.dismiss(); // Dismiss the dialog if the user didn't enter a name
-                    }
-                })
-                .setNegativeButton("Cancel", null)
-                .show();
-    }
-
-    private void setupFlashcardSetSpinner() {
-        Spinner flashcardSetSpinner = findViewById(R.id.flashcardSetSpinner);
-        flashcardSets = flashcardSetManager.getAllFlashcardSets();
-        List<String> flashcardSetNames = new ArrayList<>();
-
-        for (FlashcardSet set : flashcardSets) {
-            flashcardSetNames.add(set.getFlashcardSetName());
-        }
-
-        adapter = new ArrayAdapter<>(this, android.R.layout.simple_spinner_item, flashcardSetNames);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        flashcardSetSpinner.setAdapter(adapter);
-
-        flashcardSetSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                selectedFlashcardSet = flashcardSets.get(position);
+        new android.app.AlertDialog.Builder(this).setTitle("Create New Flashcard Set").setMessage("Enter the name for the new Flashcard Set:").setView(newSetNameInput).setPositiveButton("Create", (dialog, whichButton) -> {
+            String newSetName = newSetNameInput.getText().toString().trim();
+            if (!newSetName.isEmpty()) {
+                FlashcardSet newFlashcardSet = new FlashcardSet(newSetName);
+                flashcardSetManager.insertFlashcardSet(newFlashcardSet);
+                loadFlashcardSets(); // Refresh the list of Flashcard Sets
+            } else {
+                dialog.dismiss(); // Dismiss the dialog if the user didn't enter a name
             }
-
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-                selectedFlashcardSet = null;
-            }
-        });
-    }
-
-    private void setupSubmitButton() {
-        TextView questionTextBox = findViewById(R.id.question);
-        TextView answerTextBox = findViewById(R.id.answer);
-        TextView hintTextBox = findViewById(R.id.hint);
-        Button submitTextButton = findViewById(R.id.submitFlashcard);
-
-        questionTextBox.setOnClickListener(v -> questionTextBox.setText(""));
-        answerTextBox.setOnClickListener(v -> answerTextBox.setText(""));
-        hintTextBox.setOnClickListener(v -> hintTextBox.setText(""));
-
-        submitTextButton.setOnClickListener(v -> {
-            Flashcard flashcard = new Flashcard(answerTextBox.getText().toString(), questionTextBox.getText().toString(), hintTextBox.getText().toString());
-
-            flashcardManager.insertFlashcard(flashcard);
-            flashcardSetManager.addFlashcardToFlashcardSet(selectedFlashcardSet, flashcard);
-
-            questionTextBox.setText("");
-            answerTextBox.setText("");
-            hintTextBox.setText("");
-            loadFlashcardSets(); // Refresh the list of Flashcard Sets to update count
-
-            Toast.makeText(this, "Flashcard added successfully", Toast.LENGTH_LONG).show();
-        });
+        }).setNegativeButton("Cancel", null).show();
     }
 
     private void profilePageButtonOnClick() {
