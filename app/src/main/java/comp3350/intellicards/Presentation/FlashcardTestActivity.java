41b2c3425b629a16d956d99d5f2c837b78b3593e
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2024-06-18 17:58:30 -0500

    add inner class for modularization and giving views fields

--- app/src/main/java/comp3350/intellicards/Presentation/FlashcardTestActivity.java
+++ app/src/main/java/comp3350/intellicards/Presentation/FlashcardTestActivity.java
@@ -19,6 +19,7 @@ import comp3350.intellicards.Objects.Flashcard;
 import comp3350.intellicards.Objects.FlashcardSet;
 import comp3350.intellicards.Business.FlashcardSetManager;
 import comp3350.intellicards.Business.StubManager;
+import comp3350.intellicards.Presentation.Utils.FlashcardUtils;
 import comp3350.intellicards.R;
 
 public class FlashcardTestActivity extends Activity {
@@ -65,85 +66,148 @@ public class FlashcardTestActivity extends Activity {
     private View createView(FlashcardSet flashcardSet, Flashcard flashcard, LayoutInflater inflater, ViewFlipper viewFlipper)
     {
         View view = inflater.inflate(R.layout.flashcard_test_view, viewFlipper, false);
-        // Define click listener for the ViewHolder's View
-        TextView flashcardTextRecycle = view.findViewById(R.id.flashcardText);
-        CheckBox correctBox = view.findViewById(R.id.correctCheckbox);
-        CheckBox incorrectBox = view.findViewById(R.id.incorrectCheckbox);
-        Button nextCardButton = view.findViewById(R.id.nextCardButton);
-
-        flashcardTextRecycle.setText(flashcard.getDataFormatted());
-        setUpCheckBoxes(correctBox, incorrectBox);
-        setUpNextCardButton(flashcardSet, flashcard, nextCardButton, correctBox, incorrectBox, viewFlipper);
-
-        return view;
+        TestCardView testCardView = new TestCardView(view, viewFlipper, flashcard, flashcardSet);
+        return testCardView.getTestCardView();
     }
 
-    private void setUpCheckBoxes(CheckBox correctBox,CheckBox incorrectBox)
-    {
-        // so they can't be both checked at once
-        correctBox.setOnClickListener(v -> {
-            if (correctBox.isChecked()) {
-                incorrectBox.setChecked(false);
-            }
-        });
+    private class TestCardView {
+        //the view that we are creating
+        private View view;
+        //the flipper that we are adding too
+        private ViewFlipper viewFlipper;
+        //the flashcard information
+        private Flashcard flashcard;
+        private FlashcardSet flashcardSet;
 
-        incorrectBox.setOnClickListener(v -> {
-            if (incorrectBox.isChecked()) {
-                correctBox.setChecked(false);
-            }
-        });
+        //the associated views
+        private TextView flashcardTextView;
+        private CheckBox correctBox;
+        private CheckBox incorrectBox;
+        private Button nextCardButton;
+        private Button flipButton;
+        //check if the card should be flipped up
+        private boolean isFrontVisible;
+
+        public TestCardView(View view, ViewFlipper viewFlipper, Flashcard flashcard, FlashcardSet flashcardSet)
+        {
+            this.view = view;
+            this.viewFlipper = viewFlipper;
+            this.flashcard = flashcard;
+            this.flashcardSet = flashcardSet;
+            this.flashcardTextView = view.findViewById(R.id.flashcardText);
+            this.correctBox = view.findViewById(R.id.correctCheckbox);
+            this.incorrectBox = view.findViewById(R.id.incorrectCheckbox);
+            this.nextCardButton = view.findViewById(R.id.nextCardButton);
+            this.flipButton = view.findViewById(R.id.flipButton);
+            this.isFrontVisible = true;
+        }
+
+        private View getTestCardView()
+        {
+            setUp();
+            return view;
+        }
 
 
-    }
-    private void setUpNextCardButton(FlashcardSet flashcardSet, Flashcard flashcard, Button nextCardButton, CheckBox correctBox, CheckBox incorrectBox, ViewFlipper viewFlipper)
-    {
-        nextCardButton.setOnClickListener(v -> {
+        private void setUp()
+        {
+            setUpText();
+            setUpListeners();
+        }
 
-            int currentCardIndex = viewFlipper.getDisplayedChild();
-            int totalCardCount = viewFlipper.getChildCount();
+        private void setUpText()
+        {
+            flashcardTextView.setText(flashcard.getDataFormatted());
+        }
 
-            if (!correctBox.isChecked() && !incorrectBox.isChecked())
-            {
-                Toast.makeText(FlashcardTestActivity.this, "Please check the correct or incorrect checkbox", Toast.LENGTH_SHORT).show();
-            } else
-            {
-                if (correctBox.isChecked())
+        private void setUpListeners()
+        {
+            setUpCheckBoxes();
+            setUpNextCardButton();
+            setUpFlipButton();
+        }
+
+        private void setUpFlipButton()
+        {
+            flipButton.setOnClickListener(v -> {
+                flipFlashcard(flashcard);
+            });
+        }
+
+
+        private void setUpCheckBoxes()
+        {
+            // so they can't be both checked at once
+            correctBox.setOnClickListener(v -> {
+                if (correctBox.isChecked()) {
+                    incorrectBox.setChecked(false);
+                }
+            });
+
+            incorrectBox.setOnClickListener(v -> {
+                if (incorrectBox.isChecked()) {
+                    correctBox.setChecked(false);
+                }
+            });
+        }
+
+        private void setUpNextCardButton()
+        {
+            nextCardButton.setOnClickListener(v -> {
+
+                int currentCardIndex = viewFlipper.getDisplayedChild();
+                int totalCardCount = viewFlipper.getChildCount();
+
+                if (!correctBox.isChecked() && !incorrectBox.isChecked())
                 {
-                    flashcardManager.markAttemptedAndCorrect(flashcard);
-                    correct++;
+                    Toast.makeText(FlashcardTestActivity.this, "Please check the correct or incorrect checkbox", Toast.LENGTH_SHORT).show();
                 } else
                 {
-                    flashcardManager.markAttempted(flashcard);
-                }
+                    if (correctBox.isChecked())
+                    {
+                        flashcardManager.markAttemptedAndCorrect(flashcard);
+                        correct++;
+                    } else
+                    {
+                        flashcardManager.markAttempted(flashcard);
+                    }
 
-                if (currentCardIndex == totalCardCount - 1)
-                {
-                    viewFlipper.setVisibility(View.INVISIBLE);
-                    ReportCalculator reportCalculator = new ReportCalculator(flashcardSet);
-                    String totalReport = calculateStats(viewFlipper) + reportCalculator.report();
-                    setUpResultTextBox(totalReport);
-                } else
-                {
-                    viewFlipper.showNext();
+                    if (currentCardIndex == totalCardCount - 1)
+                    {
+                        // print out the stats of the current flashcard set test
+                        // and overall stats
+                        viewFlipper.setVisibility(View.INVISIBLE);
+                        ReportCalculator reportCalculator = new ReportCalculator(flashcardSet);
+                        String totalReport = calculateStats(viewFlipper) + reportCalculator.report();
+                        setUpResultTextBox(totalReport);
+                    } else
+                    {
+                        viewFlipper.showNext();
+                    }
                 }
-                // print out the stats of the current flashcard set test
-                // and overall stats
+            });
+        }
 
+        private void flipFlashcard(Flashcard flashcard) {
+            if (flashcard != null) {
+                FlashcardUtils.toggleFlip(flashcard, flashcardTextView, isFrontVisible);
+                isFrontVisible = !isFrontVisible;
             }
-        });
+        }
+
+        private String calculateStats(ViewFlipper viewFlipper){
+            int total = viewFlipper.getChildCount();
+            return "This tests accuracy, Correct: " + correct + " / " + total
+                    + "\nThat is " + Math.round(correct * 100 / (double)total) + "% correct\n\n";
+        }
+
+        private void setUpResultTextBox(String string)
+        {
+            TextView resultTextBox = findViewById(R.id.resultTextBox);
+            resultTextBox.setText(string);
+        }
     }
 
-    private String calculateStats(ViewFlipper viewFlipper){
-        int total = viewFlipper.getChildCount();
-        return "This tests accuracy, Correct: " + correct + " / " + total
-                + "\nThat is " + Math.round(correct * 100 / (double)total) + "% correct\n\n";
-    }
-
-    private void setUpResultTextBox(String string)
-    {
-        TextView resultTextBox = findViewById(R.id.resultTextBox);
-        resultTextBox.setText(string);
-    }
 
 
 
