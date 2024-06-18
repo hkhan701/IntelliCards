eefae7a98c5f4f2d95b0072b70adfed9fe7a62b5
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2024-06-18 02:24:43 -0500

    add accuracy report logic

---
+++ app/src/main/java/comp3350/intellicards/Business/ReportCalculator.java
@@ -0,0 +1,13 @@
+package comp3350.intellicards.Business;
+
+import comp3350.intellicards.Objects.FlashcardSet;
+
+public class ReportCalculator {
+
+    int totalActive;
+    int totalCorrect;
+
+    public ReportCalculator(FlashcardSet flashcardSet) {
+
+    }
+}
