d4f15ec48a2e92c600e550b20d9d56dcd8022f12
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-06-22 16:08:59 -0500

    Add username in profile activity
    - Refactored MainActivity, and it passes username to ProfileActivity
    - Renamed activity_account.xml to activity_profile.xml for consistency
    - ProfileActivity now displays username if logged in

--- app/src/main/java/comp3350/intellicards/Presentation/ProfileActivity.java
+++ app/src/main/java/comp3350/intellicards/Presentation/ProfileActivity.java
@@ -4,26 +4,46 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import comp3350.intellicards.R;
 
 public class ProfileActivity extends Activity {
 
+    private String username;
+
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_account);
+        setContentView(R.layout.activity_profile);
 
+        username = getIntent().getStringExtra("username"); // Get the username from the intent
+        setupBackButton();
+        setupRecoveryButton();
+        populateUsername();
+    }
+
+    private void setupBackButton() {
         Button backButton = findViewById(R.id.backButton);
-        Button recoveryButton = findViewById(R.id.recoveryButton);
         backButton.setOnClickListener(v -> {
             Intent intent = new Intent(ProfileActivity.this, MainActivity.class);
+            intent.putExtra("username", username);
             startActivity(intent);
         });
+    }
 
+    private void setupRecoveryButton() {
+        Button recoveryButton = findViewById(R.id.recoveryButton);
         recoveryButton.setOnClickListener(v -> {
             Intent intent = new Intent(ProfileActivity.this, RecoverFlashcardsActivity.class);
             startActivity(intent);
         });
     }
 
+    private void populateUsername() {
+        TextView usernameTextView = findViewById(R.id.usernameText);
+        if (username != null) {
+            usernameTextView.setText(username);
+        }
+    }
 }
