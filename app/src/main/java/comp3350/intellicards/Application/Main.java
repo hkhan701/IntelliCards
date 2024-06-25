a7ed001c59a16ce27d85f0b926a1adbcb6c0e8c2
Jade Lee <jadelee0924@gmail.com>
2024-06-25 14:11:13 -0500

    Create system's Main file

---
+++ app/src/main/java/comp3350/intellicards/Application/Main.java
@@ -0,0 +1,30 @@
+package comp3350.intellicards.Application;
+
+//import comp3350.intellicards.Presentation.CLI;
+
+public class Main
+{
+    private static String dbName="SC";
+
+    public static void main(String[] args)
+    {
+        System.out.println("All done");
+    }
+
+    public static void setDBPathName(final String name) {
+        try {
+            Class.forName("org.hsqldb.jdbcDriver").newInstance();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        }
+        dbName = name;
+    }
+
+    public static String getDBPathName() {
+        return dbName;
+    }
+}
