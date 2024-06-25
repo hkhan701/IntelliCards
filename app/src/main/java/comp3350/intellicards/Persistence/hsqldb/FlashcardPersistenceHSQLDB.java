ed1c17e25984636a04a6a6a036d5e9c9e1fd8513
Jade Lee <jadelee0924@gmail.com>
2024-06-25 14:12:27 -0500

    Create db interface implementation files

---
+++ app/src/main/java/comp3350/intellicards/Persistence/hsqldb/FlashcardPersistenceHSQLDB.java
@@ -0,0 +1,75 @@
+package comp3350.intellicards.Persistence.hsqldb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import comp3350.intellicards.Objects.Flashcard;
+import comp3350.intellicards.Persistence.FlashcardPersistence;
+
+public class FlashcardPersistenceHSQLDB implements FlashcardPersistence {
+    private final String dbPath;
+
+    public FlashcardPersistenceHSQLDB(final String dbPath) {
+        this.dbPath = dbPath;
+    }
+
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath + ";shutdown=true", "SA", "");
+    }
+
+    private Flashcard fromResultSet(final ResultSet rs) throws SQLException {
+        final String answer = rs.getString("answer");
+        final String question = rs.getString("question");
+        final String hint = rs.getString("hint");
+
+        return new Flashcard(answer, question, hint);
+    }
+
+    @Override
+    public List<Flashcard> getAllActiveFlashcards() {
+        return null;
+    }
+
+    @Override
+    public List<Flashcard> getAllDeletedFlashcards() {
+        return null;
+    }
+
+    @Override
+    public Flashcard getFlashcard(String uuid) {
+        return null;
+    }
+
+    @Override
+    public Flashcard insertFlashcard(Flashcard currentFlashcard) {
+        return null;
+    }
+
+    @Override
+    public Flashcard updateFlashcard(Flashcard currentFlashcard) {
+        return null;
+    }
+
+    @Override
+    public boolean markFlashcardAsDeleted(String uuid) {
+        return false;
+    }
+
+    @Override
+    public boolean restoreFlashcard(String uuid) {
+        return false;
+    }
+
+    @Override
+    public void markAttempted(Flashcard currentFlashcard) {
+
+    }
+
+    @Override
+    public void markAttemptedAndCorrect(Flashcard currentFlashcard) {
+
+    }
+}
---
+++ app/src/main/java/comp3350/intellicards/Persistence/hsqldb/UserPersistenceHSQLDB.java
@@ -0,0 +1,71 @@
+package comp3350.intellicards.Persistence.hsqldb;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.intellicards.Objects.User;
+import comp3350.intellicards.Persistence.UserPersistence;
+
+public class UserPersistenceHSQLDB implements UserPersistence {
+
+    private final String dbPath;
+
+    public UserPersistenceHSQLDB(final String dbPath) {
+        this.dbPath = dbPath;
+    }
+
+    private Connection connection() throws SQLException {
+        return DriverManager.getConnection("jdbc:hsqldb:file:" + dbPath + ";shutdown=true", "SA", "");
+    }
+
+    private User fromResultSet(final ResultSet rs) throws SQLException {
+        final String username = rs.getString("username");
+        final String password = rs.getString("password");
+
+        return new User(username, password);
+    }
+
+    @Override
+    public User getUser(String uuid) {
+        try (final Connection c = connection()) {
+            final Statement st = c.createStatement();
+            final ResultSet rs = st.executeQuery("SELECT * FROM users WHERE userId = '" + uuid + "'");
+            final User user = fromResultSet(rs);
+
+            rs.close();
+            st.close();
+
+            return user;
+        }
+        catch (final SQLException e) {
+            //throw new SQLException(e);
+            return null;
+        }
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        return null;
+    }
+
+    @Override
+    public void addUser(User user) {
+
+    }
+
+    @Override
+    public void updateUser(User user) {
+
+    }
+
+    @Override
+    public void deleteUser(User user) {
+
+    }
+}
