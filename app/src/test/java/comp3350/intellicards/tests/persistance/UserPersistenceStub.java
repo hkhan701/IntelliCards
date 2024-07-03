2b316f91c70742606b6054e20f9ea8c7cb13a21a
Emily Bonds <bondse@myumanitoba.ca>
2024-07-02 19:34:42 -0500

    Create independent persistence stubs for testing

---
+++ app/src/test/java/comp3350/intellicards/tests/persistance/UserPersistenceStub.java
@@ -0,0 +1,45 @@
+package comp3350.intellicards.tests.persistance;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import comp3350.intellicards.Objects.User;
+import comp3350.intellicards.Persistence.UserPersistence;
+
+public class UserPersistenceStub implements UserPersistence {
+    private List<User> users;
+
+    public UserPersistenceStub() {
+        users = new ArrayList<>();
+
+        User user1 = new User("guest", "");
+        User user2 = new User("user1", "pass1");
+        User user3 = new User("user2", "pass2");
+        User user4 = new User("user3", "pass3");
+
+        users.add(user1);
+        users.add(user2);
+        users.add(user3);
+        users.add(user4);
+    }
+
+    @Override
+    public User getUserByUsername(String username) {
+        for (User user : users) {
+            if (user.getUsername().equals(username)) {
+                return user;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public void addUser(User user) {
+        users.add(user);
+    }
+
+    @Override
+    public void deleteUser(String username) {
+        users.removeIf(u -> u.getUsername().equals(username));
+    }
+}
