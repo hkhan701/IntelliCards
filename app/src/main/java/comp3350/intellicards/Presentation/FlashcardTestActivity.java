0f3c0aa4a2447dadc596aef5a78ef9d0ff570c97
hkhan701 <96705217+hkhan701@users.noreply.github.com>
2024-07-09 17:10:28 -0500

    Fix Minor Bug fix
    Intent class not imported when merging

--- app/src/main/java/comp3350/intellicards/Presentation/FlashcardTestActivity.java
+++ app/src/main/java/comp3350/intellicards/Presentation/FlashcardTestActivity.java
@@ -1,6 +1,7 @@
 package comp3350.intellicards.Presentation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
--- app/src/main/java/comp3350/intellicards/Presentation/RecoverFlashcardsActivity.java
+++ app/src/main/java/comp3350/intellicards/Presentation/RecoverFlashcardsActivity.java
@@ -1,6 +1,7 @@
 package comp3350.intellicards.Presentation;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.appcompat.widget.AppCompatImageButton;
