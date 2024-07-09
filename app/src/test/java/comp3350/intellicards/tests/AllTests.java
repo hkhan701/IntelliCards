e56a7f6147b987bc33176ff3629fbaac40751101
Emily Bonds <bondse@myumanitoba.ca>
2024-07-08 19:15:09 -0500

    Add new testfiles to test suite

--- app/src/test/java/comp3350/intellicards/tests/AllTests.java
+++ app/src/test/java/comp3350/intellicards/tests/AllTests.java
@@ -5,16 +5,22 @@ import org.junit.runners.Suite;
 
 import comp3350.intellicards.tests.business.FlashcardManagerTest;
 import comp3350.intellicards.tests.business.FlashcardSetManagerTest;
+import comp3350.intellicards.tests.business.ReportCalculatorTest;
+import comp3350.intellicards.tests.business.UserManagerTest;
 import comp3350.intellicards.tests.objects.FlashcardSetTest;
 import comp3350.intellicards.tests.objects.FlashcardTest;
+import comp3350.intellicards.tests.objects.UserTest;
 
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-        FlashcardManagerTest.class,
-        FlashcardSetManagerTest.class,
         FlashcardTest.class,
         FlashcardSetTest.class,
+        UserTest.class,
+        FlashcardManagerTest.class,
+        FlashcardSetManagerTest.class,
+        UserManagerTest.class,
+        ReportCalculatorTest.class,
 })
 
 public class AllTests {
