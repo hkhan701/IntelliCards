2f221e645e5edaeeb175d1c185f7022ee0fc9f87
jiminryuu <144750019+jiminryuu@users.noreply.github.com>
2024-07-19 20:11:32 -0500

    add aboutMe page and buttons for profile page

---
+++ app/src/main/java/comp3350/intellicards/Presentation/AboutMeActivity.java
@@ -0,0 +1,41 @@
+package comp3350.intellicards.Presentation;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
+
+
+import comp3350.intellicards.Application.UserSession;
+import comp3350.intellicards.Business.FlashcardSetManager;
+import comp3350.intellicards.R;
+
+public class AboutMeActivity extends Activity {
+    private FlashcardSetManager flashcardSetManager;
+    private String userName;
+
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_about_me);
+
+        // Initialize flashcard set persistence
+        flashcardSetManager = new FlashcardSetManager();
+
+        // Get the user session
+        userName = UserSession.getInstance().getUsername();
+
+
+    }
+
+    private void setUpBackButton() {
+        Button backButton = findViewById(R.id.backButton);
+
+        backButton.setOnClickListener(v -> {
+            Intent intent = new Intent(comp3350.intellicards.Presentation.AboutMeActivity.this, ProfileActivity.class);
+            startActivity(intent);
+        });
+    }
+
+}
+
