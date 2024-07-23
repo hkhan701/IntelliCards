5d5fcba7f93a2b7d1f55d5ee4b19d1c2a542cd98
Emily Bonds <bondse@myumanitoba.ca>
2024-07-22 19:00:55 -0500

    Add new tests to UserTest.java

--- app/src/test/java/comp3350/intellicards/tests/objects/UserTest.java
+++ app/src/test/java/comp3350/intellicards/tests/objects/UserTest.java
@@ -18,7 +18,6 @@ public class UserTest {
     /*
      * Test getUsername()
      */
-
     @Test
     public void getUsername() {
         assertEquals("Username can be retrieved from User",
@@ -28,7 +27,6 @@ public class UserTest {
     /*
      * Test getPassword()
      */
-
     @Test
     public void getPassword() {
         assertEquals("Password can be retrieved from User",
@@ -38,7 +36,6 @@ public class UserTest {
     /*
      * Test setUsername()
      */
-
     @Test
     public void changeUsername() {
         testUser.setUsername("User1");
@@ -49,11 +46,41 @@ public class UserTest {
     /*
      * Test setPassword()
      */
-
     @Test
     public void changePassword() {
         testUser.setPassword("123");
         assertEquals("User can change their password",
                 "123", testUser.getPassword());
     }
+
+    /*
+     * Test setLoginCount()
+     */
+    @Test
+    public void changeLoginCount() {
+        testUser.setLoginCount(3);
+        assertEquals("User can change their login count",
+                3, testUser.getLoginCount());
+    }
+
+    /*
+     * Test incrementLoginCount()
+     */
+    @Test
+    public void incrementLoginCount() {
+        testUser.incrementLoginCount();
+        assertEquals("User can increment their login count",
+                1, testUser.getLoginCount());
+    }
+
+    /*
+     * Test getLoginCount()
+     */
+
+    @Test
+    public void getLoginCount() {
+        assertEquals("LoginCount can be retrieved from user",
+                0, testUser.getLoginCount());
+    }
+
 }
