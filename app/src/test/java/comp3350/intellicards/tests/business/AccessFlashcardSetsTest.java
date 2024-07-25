d96e501a7502d105f08fd1af3343cfe7bc9fc4f8
Emily Bonds <bondse@myumanitoba.ca>
2024-07-25 13:05:50 -0500

    Add Integration Tests for flashcardSet processes

--- app/src/test/java/comp3350/intellicards/tests/business/AccessFlashcardSetsTest.java
+++ app/src/test/java/comp3350/intellicards/tests/business/AccessFlashcardSetsTest.java
@@ -9,11 +9,14 @@ import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.List;
 
 import comp3350.intellicards.Application.Configuration;
 import comp3350.intellicards.Business.FlashcardSetManager;
+import comp3350.intellicards.Objects.Flashcard;
 import comp3350.intellicards.Objects.FlashcardSet;
 import comp3350.intellicards.Persistence.hsqldb.FlashcardSetPersistenceHSQLDB;
+import comp3350.intellicards.Persistence.hsqldb.PersistenceException;
 import comp3350.intellicards.tests.utils.TestUtils;
 
 public class AccessFlashcardSetsTest {
@@ -49,34 +52,85 @@ public class AccessFlashcardSetsTest {
                 flashcardSet.getFlashcardById("fc3"));
     }
 
-
-    /*
-     * getActiveFlashcardSet
-     */
-
-    /*
-     * getDeletedFlashcardSet
-     */
-
     /*
      * getAllFlashcardSets
      */
+    @Test
+    public void getAllFlashcardSets() {
+        List<FlashcardSet> flashcardSets = manager.getAllFlashcardSets();
+
+        assertEquals("all flashcard sets currently persisted can be retrieved as a list",
+                4, flashcardSets.size());
+    }
 
     /*
      * insertFlashcardSet
      */
+    @Test
+    public void testInsertFlashcard() {
+        FlashcardSet flashcardSet = new FlashcardSet("TestID", "user1","TestSubject");
+
+        manager.insertFlashcardSet(flashcardSet);
+        FlashcardSet retrievedSet = persistence.getFlashcardSet("TestID");
+
+        assertNotNull("New flashcard set can be inserted into the database",
+                retrievedSet);
+    }
+
+    @Test
+    public void testInsertFlashcardUserNotPersisted() {
+        FlashcardSet flashcardSet = new FlashcardSet("TestID", "TestUser","TestSubject");
+
+        assertThrows("New flashcard set cannot be inserted into the database if the username has not been persisted",
+                PersistenceException.class, () -> manager.insertFlashcardSet(flashcardSet));
+    }
+
+    @Test
+    public void testInsertFlashcardAssociatedData() {
+        FlashcardSet flashcardSet = new FlashcardSet("TestID", "user1","TestSubject");
+
+        manager.insertFlashcardSet(flashcardSet);
+        FlashcardSet retrievedSet = persistence.getFlashcardSet("TestID");
+
+        assumeNotNull(flashcardSet);
+
+        assertEquals("Inserting a flashcard will persist the given uuid",
+                "TestID", retrievedSet.getUUID());
+        assertEquals("Inserting a flashcard will persist the given username",
+                "user1", retrievedSet.getUsername());
+        assertEquals("Inserting a flashcard will persist the given flashcard set name",
+                "TestSubject", retrievedSet.getFlashcardSetName());
+    }
+
+    @Test
+    public void testInsertFlashcardSetAssociatedFlashcards() {
+        FlashcardSet flashcardSet = new FlashcardSet("TestID", "user1","TestSubject");
+        Flashcard flashcard = new Flashcard("testFlashcard1", "TestID", "question", "answer", "hint", false, 0, 0);
+
+        flashcardSet.addFlashcard(flashcard);
+
+        manager.insertFlashcardSet(flashcardSet);
+        FlashcardSet retrievedSet = persistence.getFlashcardSet("TestID");
+
+        assumeNotNull(flashcardSet);
+        assertNull("flashcard set will not persist associated flashcards, they should be persisted using the flashcardManager",
+                retrievedSet.getFlashcardById("testFlashcard1"));
+    }
 
     /*
-     * addFlashcardToFlashcardSet
+     * addFlashcardToFlashcardSet()
      */
+    @Test
+    public void testAddFlashcardToFlashcardSet() {
+        Flashcard flashcard = new Flashcard("id", "set2", "question", "answer", null, false, 0, 0);
 
-    /*
-     * randomizeFlashcardSet
-     */
+        manager.addFlashcardToFlashcardSet("set1", flashcard);
+        FlashcardSet retrievedSet = persistence.getFlashcardSet("set1");
 
-    /*
-     * getFlashcardSetsByUsername
-     */
+        assumeNotNull(retrievedSet);
+
+        assertNotNull(retrievedSet.getFlashcardById("id"));
+    }
 
     @After
     public void tearDown() {
