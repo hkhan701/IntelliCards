4b5ee2a625dece7b68f90195b41e9094656f42f5
Emily Bonds <bondse@myumanitoba.ca>
2024-08-02 18:35:10 -0500

    Create login method for tests

---
+++ app/src/androidTest/java/comp3350/intellicards/SearchTest.java
@@ -0,0 +1,38 @@
+package comp3350.intellicards;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+
+import androidx.test.ext.junit.rules.ActivityScenarioRule;
+import androidx.test.ext.junit.runners.AndroidJUnit4;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import comp3350.intellicards.Presentation.AuthActivity;
+
+@RunWith(AndroidJUnit4.class)
+public class SearchTest {
+    @Rule
+    public ActivityScenarioRule<AuthActivity> activityScenarioRule = new ActivityScenarioRule<>(AuthActivity.class);
+
+    private final String USERNAME = "user1";
+    private final String PASSWORD = "pass1";
+
+    public void loginUser(String username, String password) {
+        onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
+        onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
+
+        onView(withId(R.id.logInButton)).perform(click());
+    }
+
+    @Test
+    public void searchFlashcardSets() {
+        loginUser(USERNAME, PASSWORD);
+    }
+
+}
