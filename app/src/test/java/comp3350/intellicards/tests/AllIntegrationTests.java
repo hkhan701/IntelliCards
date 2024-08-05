16b0dd6e7df44062d19b18660b5d0de4d9a412d2
Emily Bonds <bondse@myumanitoba.ca>
2024-08-05 03:28:38 -0500

    Add integration tests for user processes

--- app/src/test/java/comp3350/intellicards/tests/AllIntegrationTests.java
+++ app/src/test/java/comp3350/intellicards/tests/AllIntegrationTests.java
@@ -5,10 +5,12 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 import comp3350.intellicards.tests.persistance.AccessFlashcardSetsTest;
+import comp3350.intellicards.tests.persistance.AccessUsersTest;
 
 @RunWith(Suite.class)
 @Suite.SuiteClasses({
-    AccessFlashcardSetsTest.class
+    AccessFlashcardSetsTest.class,
+    AccessUsersTest.class
 })
 
 public class AllIntegrationTests {
--- app/src/test/java/comp3350/intellicards/tests/persistance/AccessUsersTest.java
+++ app/src/test/java/comp3350/intellicards/tests/persistance/AccessUsersTest.java
@@ -1,5 +1,13 @@
 package comp3350.intellicards.tests.persistance;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeNotNull;
+import static org.junit.Assume.assumeTrue;
+
 import org.junit.Before;
 import org.junit.Test;
 
@@ -8,6 +16,7 @@ import java.io.IOException;
 
 import comp3350.intellicards.Application.Configuration;
 import comp3350.intellicards.Business.UserManager;
+import comp3350.intellicards.Objects.User;
 import comp3350.intellicards.Persistence.hsqldb.UserPersistenceHSQLDB;
 import comp3350.intellicards.tests.utils.TestUtils;
 
@@ -28,55 +37,83 @@ public class AccessUsersTest {
      * Test registerUser()
      */
     @Test
-    public void registerUser() {}
+    public void registerUser() {
+        assertTrue("New user can be registered",
+                manager.registerUser("TestUser", "TestPass"));
+    }
 
     @Test
-    public void registerUserDuplicate() {}
+    public void registerUserDuplicate() {
+        assertFalse("Cannot register new user if the wanted username is already taken",
+                manager.registerUser("user1", "pass1"));
+    }
 
     @Test
-    public void registerUserGuest() {}
+    public void registerUserGuest() {
+        assertFalse("Cannot register user with the name 'guest'",
+                manager.registerUser("guest", "guestPass"));
+    }
 
 
     /*
      * Test loginUser()
      */
     @Test
-    public void loginUserMatch() {}
+    public void loginUserMatch() {
+        User receivedUser = manager.loginUser("user1", "pass1");
+        assertNotNull("LoginUser will return user upon successful login", receivedUser);
+        assertEquals("LoginUser will return correct user upon successful login",
+                "user1", receivedUser.getUsername());
+    }
 
     @Test
-    public void loginUserFail() {}
+    public void loginUserFail() {
+        assertNull("LoginUser will not return user if username and password do not match records",
+                manager.loginUser("user1", "passTest"));
+    }
 
     @Test
-    public void loginUserDoesNotExist() {}
-
-
-    /*
-     * Test deleteUser()
-     */
-    @Test
-    public void deleteUser() {}
-
-    @Test
-    public void deleteUserNotExist() {}
-
-    @Test
-    public void deleteGuest() {}
+    public void loginUserDoesNotExist() {
+        assertNull("LoginUser will not return user if user is not persisted",
+                manager.loginUser("testUser", "testPass"));
+    }
 
     /*
      * Test incrementLoginCount()
      */
     @Test
-    public void incrementLogin() {}
+    public void incrementLogin() {
+        User retreivedUser = manager.loginUser("user1", "pass1");
+        assumeNotNull(retreivedUser);
+        assumeTrue(retreivedUser.getLoginCount() == 0);
+
+        manager.incrementLoginCount("user1");
+
+        retreivedUser = manager.loginUser("user1", "pass1");
+        assumeNotNull(retreivedUser);
+        assertEquals("User Manager can increase the login count of a user",
+                1, retreivedUser.getLoginCount());
+    }
 
     @Test
-    public void incrementLoginUserNotExist() {}
+    public void incrementLoginUserNotExist() {
+        manager.incrementLoginCount("testUser");
+        assertEquals("User Manager cannot increment the login count of a non-persisted user",
+                -1, manager.getUserLoginCount("testUser"));
+    }
 
     /*
      * Test getUserLoginCount()
      */
     @Test
-    public void getUserLoginCount() {}
+    public void getUserLoginCount() {
+        assertEquals("User Manager can retrieve a user's login count",
+                0, manager.getUserLoginCount("user1"));
+    }
 
     @Test
-    public void getUserLoginCountNotExist() {}
+    public void getUserLoginCountNotExist() {
+        assertEquals("User Manager will return a placeholder integer if user is not persisted",
+                -1, manager.getUserLoginCount("testUser"));
+    }
 }
