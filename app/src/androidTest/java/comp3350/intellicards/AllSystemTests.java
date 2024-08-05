924efd58e2ae02bf84c6ae41e4aaa737f6f9b794
Emily Bonds <bondse@myumanitoba.ca>
2024-08-05 01:38:04 -0500

    Clean System test folder - create system test test suite

---
+++ app/src/androidTest/java/comp3350/intellicards/AllSystemTests.java
@@ -0,0 +1,14 @@
+package comp3350.intellicards;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+import comp3350.intellicards.Notification.NotificationTest;
+
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        NotificationTest.class
+})
+
+public class AllSystemTests {
+}
--- app/src/androidTest/java/comp3350/intellicards/Search/SearchTest.java
+++ app/src/androidTest/java/comp3350/intellicards/Search/SearchTest.java
@@ -1,4 +1,4 @@
-package comp3350.intellicards;
+package comp3350.intellicards.Search;
 
 import static androidx.test.espresso.Espresso.onView;
 import static androidx.test.espresso.action.ViewActions.click;
@@ -6,6 +6,7 @@ import static androidx.test.espresso.action.ViewActions.closeSoftKeyboard;
 import static androidx.test.espresso.action.ViewActions.typeText;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
 
+import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 import androidx.test.ext.junit.runners.AndroidJUnit4;
 
@@ -14,6 +15,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 
 import comp3350.intellicards.Presentation.AuthActivity;
+import comp3350.intellicards.R;
 
 @RunWith(AndroidJUnit4.class)
 public class SearchTest {
@@ -24,7 +26,7 @@ public class SearchTest {
     private final String PASSWORD = "pass1";
 
     public void loginUser(String username, String password) {
-        onView(withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
+        onView(ViewMatchers.withId(R.id.username)).perform(typeText(username), closeSoftKeyboard());
         onView(withId(R.id.password)).perform(typeText(password), closeSoftKeyboard());
 
         onView(withId(R.id.logInButton)).perform(click());
@@ -35,4 +37,6 @@ public class SearchTest {
         loginUser(USERNAME, PASSWORD);
     }
 
+    @Test
+    public void searchFlashcards() {}
 }
