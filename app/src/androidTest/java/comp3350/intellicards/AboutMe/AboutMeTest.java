32127a15b4a849a3ba78dcc40198e0d3466ff91d
kgabrielson <gabrie10@myumanitoba.ca>
2024-08-06 10:07:50 -0500

    Refine 7.1 and 7.2 tests, set up initial 7.3 and 7.4 tests

--- app/src/androidTest/java/comp3350/intellicards/AboutMe/AboutMeTest.java
+++ app/src/androidTest/java/comp3350/intellicards/AboutMe/AboutMeTest.java
@@ -20,6 +20,7 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
+import comp3350.intellicards.Business.UserManager;
 import comp3350.intellicards.Presentation.AuthActivity;
 import comp3350.intellicards.R;
 import comp3350.intellicards.TestUtils.TestUtils;
@@ -43,31 +44,32 @@ public class AboutMeTest {
         USERNAME = "user71";
         PASSWORD = "pass71";
 
+        UserManager userManager = new UserManager();
+        int loginCount = userManager.getUserLoginCount("user71") + 1;
+
+        String report = "Total Flashcard Sets: 1"
+                + "\nFlashcard count: 3"
+                + "\nActive Flashcard count: 3"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: 0 / 0"
+                + "\nThat is 0% correct: "
+                + "\n\nLogin Count: " + loginCount;
+
         TestUtils.loginUserFromAuthPage(USERNAME, PASSWORD);
 
         onView(withId(R.id.headerTitle)).check(matches(allOf(isDisplayed(), ViewMatchers.withText("IntelliCards"))));
 
-        onView(withId(R.id.profileButton)).perform(click());
-        onView(withId(R.id.aboutMeButton)).perform(click());
+        onView(allOf(withId(-1), withText("set 7.1 (3) "))).perform(click());
 
-        //parse informationText [total flashcards 0]
-
-        onView(withId(R.id.backButton)).perform(click());
-        onView(withId(R.id.backButton)).perform(click());
-
-        onView(allOf(withId(-1), withText("set 7.1 (0) "))).perform(click());
-
-        onView(withId(R.id.addFlashcardButton)).perform(click());
-        onView(withId(R.id.question)).perform(typeText("test question"));
-        onView(withId(R.id.answer)).perform(typeText("answer"));
-        onView(withId(R.id.hint)).perform(typeText("helpful hint"));
-        onView(withId(R.id.createFlashcardButton)).perform(click());
+        onView(allOf(withId(R.id.flashcardsRecyclerView), withText("q1"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.flashcardsRecyclerView), withText("q2"))).check(matches(isDisplayed()));
+        onView(allOf(withId(R.id.flashcardsRecyclerView), withText("q3"))).check(matches(isDisplayed()));
 
         onView(withId(R.id.backButton)).perform(click());
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [total flashcards 1]
+        onView(withId(R.id.informationText)).check(matches(withText(report)));
 
         TestUtils.logoutUserFromAboutMePage();
     }
@@ -77,26 +79,28 @@ public class AboutMeTest {
         USERNAME = "user72";
         PASSWORD = "pass72";
 
+        UserManager userManager = new UserManager();
+        int loginCount = userManager.getUserLoginCount("user72") + 1;
+
+        String report = "Total Flashcard Sets: 2"
+                + "\nFlashcard count: 0"
+                + "\nActive Flashcard count: 0"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: 0 / 0"
+                + "\nThat is 0% correct: "
+                + "\n\nLogin Count: " + loginCount;
+
         TestUtils.loginUserFromAuthPage(USERNAME, PASSWORD);
 
         onView(withId(R.id.headerTitle)).check(matches(allOf(isDisplayed(), ViewMatchers.withText("IntelliCards"))));
 
-        onView(withId(R.id.profileButton)).perform(click());
-        onView(withId(R.id.aboutMeButton)).perform(click());
-
-        //parse informationText [total sets 0]
-
-        onView(withId(R.id.backButton)).perform(click());
-        onView(withId(R.id.backButton)).perform(click());
-
-        //add flashcard set
-        onView(withId(R.id.createNewSetButton)).perform(click());
-
+        onView(allOf(withId(-1), withText("set 7.2 A (0) "))).check(matches(isDisplayed()));
+        onView(allOf(withId(-1), withText("set 7.2 B (0) "))).check(matches(isDisplayed()));
 
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [total sets 1]
+        onView(withId(R.id.informationText)).check(matches(withText(report)));
 
         TestUtils.logoutUserFromAboutMePage();
     }
@@ -106,6 +110,25 @@ public class AboutMeTest {
         USERNAME = "user73";
         PASSWORD = "pass73";
 
+        UserManager userManager = new UserManager();
+        int loginCount = userManager.getUserLoginCount("user72") + 1;
+
+        String priorReport = "Total Flashcard Sets: 2"
+                + "\nFlashcard count: 0"
+                + "\nActive Flashcard count: 0"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: 0 / 0"
+                + "\nThat is 0% correct: "
+                + "\n\nLogin Count: " + loginCount;
+
+        String postReport = "Total Flashcard Sets: 2"
+                + "\nFlashcard count: 0"
+                + "\nActive Flashcard count: 0"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: 0 / 0"
+                + "\nThat is 0% correct: "
+                + "\n\nLogin Count: " + (loginCount + 1);
+
         TestUtils.loginUserFromAuthPage(USERNAME, PASSWORD);
 
         onView(withId(R.id.headerTitle)).check(matches(allOf(isDisplayed(), ViewMatchers.withText("IntelliCards"))));
@@ -113,7 +136,7 @@ public class AboutMeTest {
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [logins 1]
+        onView(withId(R.id.informationText)).check(matches(withText(priorReport)));
 
         TestUtils.logoutUserFromAboutMePage();
 
@@ -124,7 +147,7 @@ public class AboutMeTest {
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [logins 2]
+        onView(withId(R.id.informationText)).check(matches(withText(postReport)));
 
         TestUtils.logoutUserFromAboutMePage();
     }
@@ -134,6 +157,29 @@ public class AboutMeTest {
         USERNAME = "user74";
         PASSWORD = "pass74";
 
+        UserManager userManager = new UserManager();
+        int loginCount = userManager.getUserLoginCount("user72") + 1;
+
+        //get current counts from [somewhere]
+        int attempted = 0;
+        int correct = 0;
+
+        String priorReport = "Total Flashcard Sets: 2"
+                + "\nFlashcard count: 0"
+                + "\nActive Flashcard count: 0"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: " + correct + " / " + attempted
+                + "\nThat is " + Math.round(correct * 100 / (double) attempted) + "% correct: "
+                + "\n\nLogin Count: " + loginCount;
+
+        String postReport = "Total Flashcard Sets: 2"
+                + "\nFlashcard count: 0"
+                + "\nActive Flashcard count: 0"
+                + "\nDeleted Flashcard count: 0\n\n\n"
+                + "ALL TIME TOTAL ACCURACY\nCorrect: " + (correct+1) + " / " + (attempted + 2)
+                + "\nThat is " + Math.round((correct + 1) * 100 / (double) (attempted + 2)) + "% correct: "
+                + "\n\nLogin Count: " + loginCount;
+
         TestUtils.loginUserFromAuthPage(USERNAME, PASSWORD);
 
         onView(withId(R.id.headerTitle)).check(matches(allOf(isDisplayed(), ViewMatchers.withText("IntelliCards"))));
@@ -141,21 +187,26 @@ public class AboutMeTest {
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [accuracy report 0/0]
+        onView(withId(R.id.informationText)).check(matches(withText(priorReport)));
 
         onView(withId(R.id.backButton)).perform(click());
         onView(withId(R.id.backButton)).perform(click());
 
         onView(allOf(withId(-1), withText("set 7.4 (2) "))).perform(click());
 
-        //do a test
         onView(withId(R.id.testButton)).perform(click());
+        onView(withId(R.id.correctCheckbox)).perform(click());
+        onView(withId(R.id.nextCardButton)).perform(click());
+        onView(withId(R.id.incorrectCheckbox)).perform(click());
+        onView(withId(R.id.nextCardButton)).perform(click());
 
+        onView(withId(R.id.backButton)).perform(click());
+        onView(withId(R.id.backButton)).perform(click());
 
         onView(withId(R.id.profileButton)).perform(click());
         onView(withId(R.id.aboutMeButton)).perform(click());
 
-        //parse informationText [accuracy report 1/2]
+        onView(withId(R.id.informationText)).check(matches(withText(postReport)));
 
         TestUtils.logoutUserFromAboutMePage();
     }
--- app/src/androidTest/java/comp3350/intellicards/AboutMe/AboutMeTest.md
+++ app/src/androidTest/java/comp3350/intellicards/AboutMe/AboutMeTest.md
@@ -1,59 +1,53 @@
-# About Me System Tests
-## Total Flashcards
+# Feature 7 - About Me System Tests
+## 7.1 Total Flashcards
 1. Login as a Registered User (username: user71, password: pass71)
 2. Verify Login
-3. Select ‘profile’ button 
-4. Select ‘about me’ button
-5. Verify total flashcard count is 0
-6. Return to sets page
-7. Select ‘set 7.1’ button
-8. Create a new flashcard with fields “test question”/”answer”/”helpful hint”
-9. Confirm creation of new flashcard
-10. Return to the ‘about me’ page
-11. Verify total flashcard count is 1
-12. Click back button
-13. Click logout button
+3. Select ‘set 7.1’ button 
+4. Verify that there are 3 cards in this set 
+5. Click 'back' button 
+6. Select ‘profile’ button 
+7. Select ‘about me’ button 
+8. Verify total flashcard count is 3 
+9. Click back button 
+10. Click logout button
 
-## Total Sets
+## 7.2 Total Sets
 1. Login as a Registered User (username: user72, password: pass72)
 2. Verify Login
-3. Select ‘profile’ button
-4. Select ‘about me’ button
-5. Verify total sets is 0
-6. Return to the home page
-7. Create a new set
-8. Return to the ‘about me’ page
-9. Verify total sets is 1
-10. Click back button
-11. Click logout button
+3. Verify that the number of sets on the main page is 2 
+4. Click 'profile' button 
+5. Click 'about me' button 
+6. Verify total sets is 2 
+7. Click back button 
+8. Click logout button
 
-## Login Count
+## 7.3 Login Count
 1. Login as a Registered User (username: user73, password: pass73)
 2. Verify Login
 3. Select ‘profile’ button
 4. Select ‘about me’ button
-5. Verify total logins is 1
+5. Note the current number of logins
 6. Click back button
 7. Click logout button
 8. Login again as the same registered user (username: user73, password: pass73)
 9. Verify Login
 10. Select ‘profile’ button 
 11. Select ‘about me’ button
-12. Verify total logins is 2
+12. Verify total logins has increased by 1
 13. Click back button
 14. Click logout button
 
-## Accuracy Report
+## 7.4 Accuracy Report
 1. Login as a Registered User (username: user74, password: pass74)
 2. Verify Login
 3. Select ‘profile’ button 
 4. Select ‘about me’ button
-5. Verify accuracy report is 0/0
+5. Note the current accuracy report (a/b)
 6. Return to home page
 7. Select ‘set 7.4’
 8. Select ‘test’ button
 9. Answer 1 card correctly, and 1 incorrectly
 10. Return to ‘about me’ page
-11. Verify accuracy report is 1/2
+11. Verify accuracy report is (a+1)/(b+2)
 12. Click back button
 13. Click logout button
